7fc1be1d92f9016b78b829d71ca92ab22ebedd1d
Deepak Kapor <DEEPAK.KAPOR@star.com.au>
2025-01-09 21:45:12 +1100

    udpate nvim config

---
+++ nvim/.config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    version = " v14.6.0", -- Pin to the desired version
+    dependencies = {
+      -- Specify LazyVim dependencies here if needed
+    },
+  },
+}
